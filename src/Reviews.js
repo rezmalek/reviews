7f036c793ec75b916ade2e4c29609c1ebc13a534
RezMalek <rezmalek7@gmail.com>
2021-04-07 16:41:32 -0700

    add prev next functions

--- src/Reviews.js
+++ src/Reviews.js
@@ -8,6 +8,32 @@ function Reviews() {
   // everytime the value of the index changes below destructured properties change so will the person
   const { id, name, job, image, text } = people[index];
 
+  // switch from last review to first and vice versa
+  const checkIndex = (index) => {
+    if (index > people.length - 1) {
+      return 0;
+    }
+    if (index < 0) {
+      return people.length - 1;
+    }
+    return index;
+  };
+
+  // prev and next functions
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkIndex(newIndex);
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkIndex(newIndex);
+    });
+  };
+
   return (
     <article className='review'>
       <div className='img-container'>
@@ -20,10 +46,10 @@ function Reviews() {
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
       <div className='button-container'>
-        <button className='prev-btn'>
+        <button className='prev-btn' onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className='next-btn'>
+        <button className='next-btn' onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
