de7d625af2e8369eae3b6c199601d2fb356c3942
RezMalek <rezmalek7@gmail.com>
2021-04-07 16:25:18 -0700

    setup reviews

---
+++ src/Reviews.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+function Reviews() {
+  const [index, setIndex] = useState(0);
+
+  // everytime the value of the index changes below destructured properties change so will the person
+  const { id, name, job, image, text } = people[index];
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>Random Review</button>
+    </article>
+  );
+}
+
+export default Reviews;
